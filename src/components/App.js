deec44ef0a4c1f3ac5da3338d1cc487e836e9865
ronnysang <ronny.sang@student.moringaschool.com>
2025-10-15 15:47:48 +0300

    complete react lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
-import itemData from "../data/items";
+import itemsData from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
+  const [items, setItems] = useState(itemsData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleAddItem} />
     </div>
   );
 }
